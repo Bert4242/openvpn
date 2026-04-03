12a01280e1f87d500b778d39101484a065c5f567
Temp User <temp@example.com>
2026-04-04 00:34:43 +0200

    u

--- src/openvpn/socket.c
+++ src/openvpn/socket.c
@@ -2235,17 +2235,10 @@ stream_buf_added(struct stream_buf *sb, int length_added)
                         /* SNI header not found  */
                         if (sb->sni_passthrough_state == SNI_PT_PENDING)
                         {
-                            /* assuming SNI header will always be in the first packet complete  , THAT IS NOT TRUE !!!!!!!!!!!!!!!!!!!!!!!!*/
-    //                        sb->sni_passthrough_state = SNI_PT_DISABLED;
-    return false;
+                            /* wait for more data until sni_passthrough_consume_header or is_openvpn_protocol() match */
+                            return false;
                         }
-                        
-/*
-should not return , no ? 
-return false;
-*/
                     }
-
                 }
 #endif
                 if (sb->port_share_state == PS_ENABLED )
