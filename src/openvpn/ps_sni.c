45506c04fae42d1d0294235065dd5af2c70fb4ae
Temp User <temp@example.com>
2026-04-04 14:05:11 +0200

    e

--- src/openvpn/ps_sni.c
+++ src/openvpn/ps_sni.c
@@ -117,9 +117,9 @@ sni_passthrough_build_client_hello(uint8_t *buf, size_t bufsz, const char *sni)
     int handshake_ret = SSL_do_handshake(ssl);
     if (handshake_ret != 1)
     {
+        int ssl_err = SSL_get_error(ssl, handshake_ret);
         if (ssl_err != SSL_ERROR_WANT_READ && ssl_err != SSL_ERROR_WANT_WRITE)
         {
-            int ssl_err = SSL_get_error(ssl, handshake_ret);
             char err_buf[256];
             ERR_error_string_n(ERR_get_error(), err_buf, sizeof(err_buf));
             msg(M_NONFATAL, "--sni-passthrough-hostname: SSL_do_handshake failed: %s", err_buf);
